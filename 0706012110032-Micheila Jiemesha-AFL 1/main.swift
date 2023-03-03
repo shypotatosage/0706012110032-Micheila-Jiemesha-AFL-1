c084b8b56ac4d6e4e0af7f1288fbeb074ddcaff7
shypotatosage <micheilajiemesha@gmail.com>
2023-03-03 07:32:45 +0700

    Waiting TBA

--- 0706012110032-Micheila Jiemesha-AFL 1/main.swift
+++ 0706012110032-Micheila Jiemesha-AFL 1/main.swift
@@ -88,23 +88,53 @@ repeat {
     } else if (choice.lowercased() == "h") {
         var hchoice: String = ""
         
-        print("""
-        Your HP is \(HP).
-        You have \(potion) Potions.
-        
-        """)
-        
-        repeat {
-            print("Are you sure you want to use 1 potion to heal wound? [Y/N] ", terminator: "")
+        if potion > 0 {
+            print("""
+            \nYour HP is \(HP).
+            You have \(potion) Potions.
             
-            hchoice = readLine()!
-        } while hchoice.lowercased() != "y" && hchoice.lowercased() != "n"
-        
-        if (hchoice.lowercased() == "y") {
-            HP += 20
-            potion -= 1
+            """)
+            
+            repeat {
+                print("Are you sure you want to use 1 potion to heal wound? [Y/N] ", terminator: "")
+                
+                hchoice = readLine()!
+            } while hchoice.lowercased() != "y" && hchoice.lowercased() != "n"
+            
+            if (hchoice.lowercased() == "y") {
+                HP += 20
+                potion -= 1
+                
+                if (HP > 100) {
+                    HP = 100
+                }
+            }
+            
+            while hchoice.lowercased() != "n" {
+                print("""
+                \nYour HP is \(HP) now.
+                You have \(potion) Potions left.
+                
+                """)
+                
+                repeat {
+                    print("Still want to use 1 potion to heal wound again? [Y/N] ", terminator: "")
+                    
+                    hchoice = readLine()!
+                } while hchoice.lowercased() != "y" && hchoice.lowercased() != "n"
+            }
+        } else {
+            print("You don't have any potion left. Be careful of your next journey.")
+            
+            repeat {
+                print("Press [return] to go back: ", terminator: "")
+                
+                hchoice = readLine()!
+            } while hchoice != ""
         }
     } else if (choice.lowercased() == "f") {
+        var fchoice: String = ""
+        
         print("""
         As you enter the forest, you feel a sense of unease wash over you.
         Suddenly, you hear the sound of twigs snapping behind you. You quickly spin around, and find a Troll emerging from the shadows.
@@ -112,6 +142,74 @@ repeat {
         😈 Name: Troll x1
         😈 Health: 1000
         
+        Choose your action:
+        [1] Physical Attack. No mana required. Deal 5pt of damage.
+        [2] Meteor. Use 15pt of MP. Deal 50pt of damage.
+        [3] Shield. Use 10pt of MP. Block enemy's attack in 1 turn.
+        
+        [4] Use Potion to heal wound.
+        [5] Scan enemy's vital.
+        [6] Flee from battle.
+        
         """)
+        
+        repeat {
+            print("Your choice? ", terminator: "")
+            fchoice = readLine()!
+        } while fchoice.range(of: "[^1-6]", options: .regularExpression) != nil
+        
+        if (fchoice == "6") {
+            print("""
+            \nYou feel that if you don't escape soon, you won't be able to continue the fight.
+            You look around frantically, searching for a way out. You sprint towards the exit, your heart pounding in your chest.
+            
+            You're safe, for now.
+            """)
+            
+            repeat {
+                print("Press [return] to go back: ", terminator: "")
+                
+                fchoice = readLine()!
+            } while fchoice != ""
+        }
+    } else if (choice.lowercased() == "m") {
+        var mchoice: String = ""
+        
+        print("""
+        As you make your way through the rugged mountain terrain, you can feel the chill of the wind biting at your skin. Suddenly, you hear a sound that makes you freeze in your tracks. That's when you see it - a massive, snarling Golem emerging from the shadows.
+        
+        😈 Name: Golem x1
+        😈 Health: 1000
+        
+        Choose your action:
+        [1] Physical Attack. No mana required. Deal 5pt of damage.
+        [2] Meteor. Use 15pt of MP. Deal 50pt of damage.
+        [3] Shield. Use 10pt of MP. Block enemy's attack in 1 turn.
+        
+        [4] Use Potion to heal wound.
+        [5] Scan enemy's vital.
+        [6] Flee from battle.
+        
+        """)
+        
+        repeat {
+            print("Your choice? ", terminator: "")
+            mchoice = readLine()!
+        } while mchoice.range(of: "[^1-6]", options: .regularExpression) != nil
+        
+        if (mchoice == "6") {
+            print("""
+            \nYou feel that if you don't escape soon, you won't be able to continue the fight.
+            You look around frantically, searching for a way out. You sprint towards the exit, your heart pounding in your chest.
+            
+            You're safe, for now.
+            """)
+            
+            repeat {
+                print("Press [return] to go back: ", terminator: "")
+                
+                mchoice = readLine()!
+            } while mchoice != ""
+        }
     }
 } while choice.lowercased() != "q"
